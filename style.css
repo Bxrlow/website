d36c90160f5dd2631d2f4952079d951bb22b1072
Bxrlow <joshuabarlow32@gmail.com>
2015-04-30 15:33:02 +0100

    Create style.css

---
+++ style.css
@@ -0,0 +1,99 @@
+body
+{
+	margin: 0;
+	padding: 0;
+	background-color: #FFFF66;
+}	
+
+#header
+{
+	width: 100%;
+	height: 100px;
+	background-color: #363636;
+}
+
+#container
+{
+	margin: auto;
+	width: 960px;
+	height: auto;
+}
+
+#content
+{
+	width: 945px;
+	height: 1600px;
+	margin-top: 50px;
+	margin-bottom: 50px;
+	padding: 15px;
+	border-radius: 15px;
+	background-color: #FEFEFE;
+	font-family: Agency FB;
+	font-size: 20px;
+	font-weight: bold;
+	text-align: center;
+}
+
+#footer
+{
+	bottom: 0;
+	position: relative;
+	width: 100%;
+	height: 100px;
+	background-color: #363636;
+}
+
+#footerbody {
+	margin: 0 auto;
+	color: #FF6666;
+}
+
+#navigation
+{
+	margin: auto;
+	width: 960px;
+	height: 100px;
+	background-color: #363636;
+}
+
+#navigation li
+{
+	margin: auto;
+	float: left;
+	position: relative;
+}
+
+#navigation a
+{
+	float: left;
+	padding: 35px;
+	color: #FFFF66;
+	font-family: Agency FB;
+	font-size: 12px;
+	font-weight: bold;
+	text-decoration: none;
+	text-transform: uppercase;
+}
+
+#navigation li.active a,
+#navigation li:hover > a
+{
+	color: #363636;
+	background-color: #FFFF66;
+	height: 65px;
+}
+
+.headertext
+{
+	width: 600px;
+	margin: auto;
+	font-family: Agency FB;
+	font-size: 30px;
+	font-weight: bold;
+	padding-top: 15px;
+	padding-bottom: 15px;
+	margin-top: 15px;
+	margin-bottom: 15px;
+	border-top: 1px solid #363636;
+	border-bottom: 1px solid #363636;
+}	
